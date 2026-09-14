4561887ab9b37b3298180223ecfb9725f6be4934
ashishgitrepo1 <ashish.turbobit@gmail.com>
2026-09-14 23:44:47 +0530

    1005 leetcode

--- 1005_leetcode.js
+++ 1005_leetcode.js
@@ -1,3 +1,36 @@
+let Swap = function(nums, left, right)
+{
+    let temp = nums[right];
+    nums[right] = nums[left];
+    nums[left] = temp;
+}
+let getPartition = function(nums, left, right, pivot)
+{
+    let leftPtr = left;
+    let rightPtr = right - 1;
+
+    while (true)
+    {
+        while (nums[leftPtr] < pivot)
+        {
+            leftPtr += 1;
+        }
+        while (nums[rightPtr] >= pivot)
+        {
+            rightPtr -= 1;
+        }
+        if (leftPtr > rightPtr)
+        {
+            break;
+        }
+        else
+        {
+            Swap(nums, leftPtr, rightPtr);
+        }
+    }
+    Swap(nums, leftPtr, right);
+    return leftPtr;
+}
 let QuickSort = function(nums, left, right)
 {
     if (right-left <=0 )
@@ -6,14 +39,18 @@ let QuickSort = function(nums, left, right)
     }
     else
     {
-        
+        let pivot = nums[right];
+        let partition = getPartition(nums, left, right, pivot);
+        QuickSort(nums, left, partition-1);
+        QuickSort(nums, partition+1, right);
     }
 }
 var largestSumAfterKNegations = function(nums, k) {
     let maxSum = 0;
     // nums.sort((a,b) => a-b);
     QuickSort(nums,0, nums.length-1);
-    console.log(nums);
+    
+    // console.log(nums);
     
     let n = nums.length;
     
